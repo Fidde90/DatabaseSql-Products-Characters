d8fada4cee555cdb4f2baf904bad8e283280bd94
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-02 16:05:58 +0100

    Packages installed

---
+++ Infrastructure/Entities/CategoryEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Infrastructure.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Entities
+{
+    [Index(nameof(CategoryName), IsUnique = true)]
+    public class CategoryEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string CategoryName { get; set; } = null!;
+
+        public virtual ICollection<ProductEntity> Products { get; set; } = new HashSet<ProductEntity>();
+
+        public static implicit operator CategoryEntity(Product product)
+        {
+            CategoryEntity specification = new CategoryEntity
+            {
+                CategoryName = product.Category       
+            };
+
+            return specification;
+        }
+    }
+}
