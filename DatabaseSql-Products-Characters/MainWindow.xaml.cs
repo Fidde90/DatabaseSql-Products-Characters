b0bc507c168601ca1f70d1a683ff77a5c52ca792
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-02 15:41:36 +0100

    Add project files.

---
+++ DatabaseSql-Products-Characters/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DatabaseSql_Products_Characters
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
